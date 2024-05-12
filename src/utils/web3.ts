3d133cc6b5f94d8dd905e09e2f01822ea19019fe
tothetop430 <jhuseyrer355545@gmail.com>
2024-05-12 15:50:43 -0400

    add: web3 part

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { WalletContextState, useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { program } from "../anchor/setup";
+import { IDL, LaunchnftContract } from "../anchor/idl";
 import { Connection, PublicKey } from "@solana/web3.js";
-import { Wallet } from "@coral-xyz/anchor";
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
  
+const programId = new PublicKey("HNztz1uSj4fyUfSnaJLeAtmf9pMqnmAFTJArNwBQhfqU");
+
+export function GetLaunchpadProgram(
+  wallet: WalletContextState,
+  connection: Connection,
+){
+  const provider = new AnchorProvider(connection, wallet, {});
+  const program = new Program<LaunchnftContract>(IDL, programId, provider);
+  return program;
+}
 export default async function Initialize(
     wallet: WalletContextState,
     connection: Connection,
@@ -15,8 +25,10 @@ export default async function Initialize(
   if (!wallet.publicKey) return;
  
     try {
+      const program = GetLaunchpadProgram(wallet, connection);
+      
       // Create a transaction to invoke the increment function
-      const transaction = await program.methods
+      const transactionSignature = await program.methods
         .initialize({
             feeWallet: feeWallet,
             feePercent: feePercent
@@ -25,12 +37,7 @@ export default async function Initialize(
             authority: wallet.publicKey,
             launchpad: launchpadPda,
         })
-        .transaction();
- 
-      const transactionSignature = await wallet.sendTransaction(
-        transaction,
-        connection,
-      );
+        .rpc();
  
       console.log(
         `initialize tx id: ${transactionSignature}`,
@@ -41,7 +48,14 @@ export default async function Initialize(
     }
  
 }
-
+export async function GetLaunchpad(
+  wallet: WalletContextState,
+  connection: Connection,
+){
+  const program = GetLaunchpadProgram(wallet, connection);
+  const launchpad = await program.account.launchpad.fetchNullable(launchpadPda);
+  return launchpad;
+}
 export async function Update(
     wallet: WalletContextState,
     connection: Connection,
@@ -51,8 +65,10 @@ export async function Update(
   if (!wallet.publicKey) return;
  
     try {
+      const program = GetLaunchpadProgram(wallet, connection);
+
       // Create a transaction to invoke the increment function
-      const transaction = await program.methods
+      const transactionSignature = await program.methods
         .update({
             feeWallet: feeWallet,
             feePercent: feePercent
@@ -61,12 +77,8 @@ export async function Update(
             authority: wallet.publicKey,
             launchpad: launchpadPda,
         })
-        .transaction();
+        .rpc();
  
-      const transactionSignature = await wallet.sendTransaction(
-        transaction,
-        connection,
-      );
  
       console.log(
         `update tx id: ${transactionSignature}`,
@@ -79,5 +91,33 @@ export async function Update(
 }
 export const [launchpadPda] = PublicKey.findProgramAddressSync(
     [Buffer.from("launchpad")],
-    program.programId,
+    programId,
   );
+
+export async function CreateProject(
+  wallet: WalletContextState,
+  connection: Connection,
+){
+  try {
+    const program = GetLaunchpadProgram(wallet, connection);
+    
+    // Create a transaction to invoke the increment function
+    // const transactionSignature = await program.methods
+    //   .createProject({
+    //       feeWallet: feeWallet,
+    //       feePercent: feePercent
+    //   })
+    //   .accounts({
+    //       authority: wallet.publicKey,
+    //       launchpad: launchpadPda,
+    //   })
+    //   .rpc();
+
+    // console.log(
+    //   `initialize tx id: ${transactionSignature}`,
+    // );
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+}
--- src/views/AdminView.tsx
+++ src/views/AdminView.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from 'react';
 import { FloatingLabel, Button } from "flowbite-react";
 // import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 // import { program } from "../anchor/setup";
-import Initialize, { Update } from 'utils/web3';
+import Initialize, { GetLaunchpad, Update } from 'utils/web3';
 import { PublicKey } from '@solana/web3.js';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 
@@ -13,30 +13,41 @@ interface AdminType {
 export const AdminView: FC<AdminType> = ({ }) => {
     const wallet = useWallet();
     const { connection } = useConnection();
-    const [adminWallet, setAdminWallet] = useState('');
     const [feeWallet, setFeeWallet] = useState('');
     const [feeAmount, setFeeAmount] = useState(0);
     
 
+    const [launchpad, setLaunchpad] = useState({} as any);
+
+    useEffect(() => {
+        GetLaunchpad(wallet, connection).then((value)=>{
+            if(value){
+                setLaunchpad(value);
+                setFeeWallet(value.feeWallet.toBase58());
+                setFeeAmount(value.feePercent / 100);
+            }
+            
+        })
+    }, [])
+
     const on_click_CreateAdminWallet = async () => {
         console.log("clicked")
-        await Initialize(wallet, connection, new PublicKey(Buffer.from(feeWallet)), feeAmount);
+        await Initialize(wallet, connection, new PublicKey(feeWallet), feeAmount * 100);
     }
 
     const on_click_UpdateAdminWallet = async () => {
-        await Update(wallet, connection, new PublicKey(Buffer.from(feeWallet)), feeAmount);
+        await Update(wallet, connection, new PublicKey(feeWallet), feeAmount * 100);
     }
 
     return (
         <div className='flex flex-col justify-center items-center m-4 w-full gap-3'>
-            <FloatingLabel variant="outlined" label="Admin Wallet" name='adminWallet' value={adminWallet} onChange={(e)=>setAdminWallet(e.target.value)} />
             <FloatingLabel variant="outlined" label="Fee Wallet" name='feeWallet' value={feeWallet} onChange={(e)=>setFeeWallet(e.target.value)} />
             <FloatingLabel variant="outlined" label="Fee Amount" name='feeAmount' value={feeAmount} onChange={(e)=>setFeeAmount(parseInt( e.target.value.trim()) )} />
             <div className='flex flex-row justify-center gap-4'>
-                <Button outline gradientDuoTone="greenToBlue" onClick={() => {on_click_CreateAdminWallet}}>
+                <Button outline gradientDuoTone="greenToBlue" onClick={(e) => {on_click_CreateAdminWallet()}}>
                     Create Admin Panel
                 </Button>
-                <Button outline gradientDuoTone="greenToBlue" onClick={() => on_click_UpdateAdminWallet}>
+                <Button outline gradientDuoTone="greenToBlue" onClick={(e) => on_click_UpdateAdminWallet()}>
                     Update Admin Panel
                 </Button>
             </div>
