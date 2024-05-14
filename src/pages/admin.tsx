7f49093bf571278db5a52bcfe05d5cc4117b2e0c
tothetop430 <jhuseyrer355545@gmail.com>
2024-05-14 13:39:38 -0700

    update admin.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -5,7 +5,7 @@ import { AdminView } from '../views/AdminView';
 const Home: NextPage = (props) => {
     return (
         <div>
-            <Head children={""}>
+            <Head>
                 <title>NFT Minter</title>
                 <meta
                     name="description"
